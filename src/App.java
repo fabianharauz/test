37029548c2f70f216c35c31b926e5fbcbba598e1
iangl2 <93356017+iangl2@users.noreply.github.com>
2024-10-03 22:21:25 -0500

    This is the first commit of Branch  Ian

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
+
+    static void ian(){
+        System.out.println("Hello my name is ian");
+
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello my name is fabian");
+        ian();
     }
 }
