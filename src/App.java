04bac73ee9f920ac7cde2bf75c97c42ae2e84b35
EdgarDaza <edgar.daza@utp.ac.pa>
2024-10-03 22:14:07 -0500

    Cambios de nombre

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello my name is fabian");
+        System.out.println("Hello my name is Edgar");
     }
 }
